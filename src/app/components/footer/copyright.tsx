89369326f3175032d7bfd41d4f71841ab87bc107
Eldar Ziiatdinov <ziatdinov.eldar9@gmail.com>
2019-10-13 00:55:54 +0400

    added styles and links to svg in footer
    Issue: #11

--- src/app/components/footer/copyright.tsx
+++ src/app/components/footer/copyright.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import styles from './footer.scss';
 import { Icon } from '../icon';
 
-export const Copyright = () => {
-  return (
-    <div className={styles.copyright}>
-      <span>{`\u00A9 Wakeclub ${new Date().getFullYear()}`}</span>
-      <Icon.Profile />
-      <Icon.Notification />
-      <Icon.Instagram />
-      <Icon.Telegram />
-      <Icon.Vk />
+//todo: ссылки на группы инстаграм итд вынести как параметры которые модно будет задать
+export const Copyright = () => (
+  <div className={styles.footerLegal}>
+    <span className={styles.copyright}>{`\u00A9 Wakeclub ${new Date().getFullYear()}`}</span>
+    <div className={styles.socialLinks}>
+      <a href={'https://www.instagram.com/wake_63'} target={'_blank'}>
+        <Icon.Instagram />
+      </a>
+      <a href={'https://www.instagram.com/wake_63'} target={'_blank'}>
+        <Icon.Telegram />
+      </a>
+      <a href={'https://vk.com/wake63'} target={'_blank'}>
+        <Icon.Vk />
+      </a>
     </div>
-  );
-};
+  </div>
+);
