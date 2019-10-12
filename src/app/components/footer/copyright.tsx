263a37892f67d8d519502013fe3db6c3d94f61d9
Eldar Ziiatdinov <ziatdinov.eldar9@gmail.com>
2019-10-12 15:35:59 +0400

    added svg and svg-components
    Issue: #11

--- src/app/components/footer/copyright.tsx
+++ src/app/components/footer/copyright.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import styles from './footer.scss';
+import { Icon } from '../icon';
 
 export const Copyright = () => {
-  return <div className={styles.copyright}>{`\u00A9 Wakeclub ${new Date().getFullYear()} `}</div>;
+  return (
+    <div className={styles.copyright}>
+      <span>{`\u00A9 Wakeclub ${new Date().getFullYear()}`}</span>
+      <Icon.Profile />
+      <Icon.Notification />
+      <Icon.Instagram />
+      <Icon.Telegram />
+      <Icon.Vk />
+    </div>
+  );
 };
---
+++ src/app/components/icon/index.ts
@@ -0,0 +1,13 @@
+import { ProfileIcon } from './profile';
+import { NotificationIcon } from './notification';
+import { InstagramIcon } from './instagram';
+import { TelegramIcon } from './telegram';
+import { VkIcon } from './vk';
+
+export const Icon = {
+  Profile: ProfileIcon,
+  Notification: NotificationIcon,
+  Instagram: InstagramIcon,
+  Telegram: TelegramIcon,
+  Vk: VkIcon,
+};
