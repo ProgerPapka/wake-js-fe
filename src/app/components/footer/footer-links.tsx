f4d66730870697f84d4f3f927a78abb5b83d3400
Eldar Ziiatdinov <ziatdinov.eldar9@gmail.com>
2019-10-13 02:43:07 +0400

    added links-map in footer
    Issue: #11

--- src/app/components/footer/footer-links.tsx
+++ src/app/components/footer/footer-links.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import styles from './footer.scss';
+import { Logo } from '../logo/logo';
+import { Link } from '../link';
 
 export const FooterLinks = () => {
-  return <div className={styles.footerLinks}></div>;
+  return (
+    <div className={styles.footerLinks}>
+      <div className={styles.footerLinksLogo}>
+        <Logo secondaryLogo={true} />
+      </div>
+      <div className={`${styles.footerLinksMap} ${styles.containerLinks}`}>
+        <nav className={styles.containerLinksMap}>
+          <div className={styles.groupLinks}>
+            <Link pathTo={'/'} value={'Главная'} />
+            <Link pathTo={'/price'} value={'Услуги'} />
+            <Link pathTo={'/vacancies'} value={'Вакансии'} />
+            <Link pathTo={'/contacts'} value={'Контакты'} />
+          </div>
+        </nav>
+        <nav className={styles.containerLinksTechnical}>
+          <div className={styles.groupLinks}>
+            <Link pathTo={'/support'} value={'Обратиться в поддерку'} />
+            <Link pathTo={'/wishes'} value={'Оставить пожелание'} />
+          </div>
+        </nav>
+        <div className={styles.containerLinksUpstair}>
+          <a className={styles.upstair}>Наверх</a>
+        </div>
+      </div>
+    </div>
+  );
 };
